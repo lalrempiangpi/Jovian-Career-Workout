6f3880d38c18d462ccf1c1180bbba0b412a1c4a8
LalRemPiang <124562293+lalrempiangpi@users.noreply.github.com>
2023-02-21 10:45:12 +0000

    Added some more changes such as JOBS list in Opening Position

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+ {
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Chinmai, Thailand',
+  'salary': 'Kyats. 700,000'
+ },
+ {
+  'id': 1,
+  'title': 'Data Scientist',
+  'location': 'Beijein, China',
+  'salary': 'Kyats. 500,000'
+ },
+ {
+  'id': 1,
+  'title': 'Frontend Engineer',
+  'location': 'Ohio, Frence',
+  'salary': 'Kyats. 700,000'
+ },
+ {
+  'id': 1,
+  'title': 'Backend Engineer',
+  'location': 'Hong Kong, China',
+  'salary': 'Kyats. 700,000'
+ },
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+	return render_template('home.html', jobs=JOBS,)
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
